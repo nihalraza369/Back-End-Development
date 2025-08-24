5756919873c1eda4daf6c04ead97ac459754d51b
nihalraza369 <nihalraza369@gmail.com>
2025-08-24 23:31:01 +0500

    expressjs practice

---
+++ Expressjs/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use((req, res, next) => {
+    console.log('pehla Middleware function chalu hai');
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+app.use((req, res, next) => {
+    console.log('Dusra Middleware function chalu hai');
+    next();
+});
+
+app.get('/about', function (req, res, next) {
+    // Yahan error bhej rahe hain
+    next(new Error('Something went wrong'));
+});
+
+// Error handling middleware (4 parameters zaroori hain!)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
