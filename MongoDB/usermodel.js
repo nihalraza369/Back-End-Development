27fad466c91d3430ae1a988b729bb5afcfa12c24
nihalraza369 <nihalraza369@gmail.com>
2025-08-26 20:04:37 +0500

    mongoDB

---
+++ MongoDB/usermodel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/mydatabase');
+
+const userschema = mongoose.Schema({
+  name: String,
+  email: String,
+  username: String
+});
+
+module.exports = mongoose.model('User', userschema);
