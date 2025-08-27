63c3087553202eacb329a6e33936b3f627c2d632
nihalraza369 <nihalraza369@gmail.com>
2025-08-27 17:45:04 +0500

    mongodb practice

---
+++ MongoDB project/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/testapp1")
+
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    image: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
